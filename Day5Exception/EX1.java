1bd0cedcc36118482b07fb305d3a894e37ea928a
Yogita Mohite <yogitamohite226@gmail.com>
2025-11-23 18:00:27 +0530

    Added collection program

---
+++ Day5Exception/EX1.java
@@ -0,0 +1,16 @@
+package Day5Exception;
+
+public class EX1 {
+    public static void main(String[] args) {
+        try {
+            int a=10;
+            int b=0;
+            int result=a/b;
+            System.out.println("Result:" +result);
+        } catch (Exception e) {
+            System.out.println("Error occured:" +e.getMessage());
+        }
+        System.out.println("program continues...");
+    }
+    
+}
